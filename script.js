b3d8dc155a895b22a680fd23d02a4b7295044085
AthrvTech <mrhackershivansh143@gmail.com>
2025-06-08 00:49:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-function toggleSidebar() {
-  const sidebar = document.getElementById('sidebar');
-  sidebar.style.display = sidebar.style.display === 'flex' ? 'none' : 'flex';
-}
+const menuToggle = document.getElementById('menuToggle');
+const sidebar = document.getElementById('sidebar');
+
+// Toggle sidebar on click
+menuToggle.addEventListener('click', () => {
+  sidebar.style.right = '0';
+});
+
+// Hide sidebar when clicked outside
+document.addEventListener('click', function (e) {
+  if (!sidebar.contains(e.target) && !menuToggle.contains(e.target)) {
+    sidebar.style.right = '-250px';
+  }
+});
